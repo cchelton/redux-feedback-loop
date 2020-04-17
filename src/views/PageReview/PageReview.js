431c1f6745f2a7192b3d775296a939cfe9a57da8
cchelton <59346941+cchelton@users.noreply.github.com>
2020-04-17 16:35:26 -0500

    adds this page, see previous commit for more deets

---
+++ src/views/PageReview/PageReview.js
@@ -0,0 +1,1 @@
+// review and submit here. links back to previous pages. probably will need to reset redux store on submit
---
+++ src/views/PageThanks/PageThanks.js
@@ -0,0 +1,2 @@
+//  display thank you message for completing feedback and have button to restart whole feedback process.
+//  probably should clear the store on submit
