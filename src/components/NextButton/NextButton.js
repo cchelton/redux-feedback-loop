6644802a58d0d5f788586c0e945f250a155a0e56
cchelton <59346941+cchelton@users.noreply.github.com>
2020-04-17 16:30:08 -0500

    adds file layout
    I used this commit to create a, initial map of all the views and components I think I'll be needing.
    
    I'll probably add DeleteButton and SubmitButton. Maybe a dynamic button component that takes it's handler func as props.
    
    Other components to be added as necessary.

---
+++ src/components/NextButton/NextButton.js
@@ -0,0 +1,1 @@
+// single purpose button, do not submit, just advance. let redux hold inputs until submit
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App/App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
+
+//  redux needs to hold inputs to allow for future edits.
+//  submit at review view, next buttons just advance to next view
+//  on review screen use router to redirect user to the view they want to edit (feeling, understanding, ss, comments)
+//  server needs routing and pool setup
+//  ss in this project is "staff support" not the german abbr.
