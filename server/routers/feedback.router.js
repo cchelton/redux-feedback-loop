15113b92d88ef7cde40193fc08665bbd96b423f7
cchelton <59346941+cchelton@users.noreply.github.com>
2020-04-18 13:32:47 -0500

    adds POST route to /feedback

--- server/routers/feedback.router.js
+++ server/routers/feedback.router.js
@@ -17,4 +17,33 @@ router.get("/", (req, res) => {
     });
 });
 
+router.post("/", (req, res) => {
+  console.log(`In /feedback POST`);
+
+  let newFeedback = req.body;
+  let queryText = "";
+
+  newFeedback = [
+    newFeedback.feeling,
+    newFeedback.understanding,
+    newFeedback.support,
+    newFeedback.comments,
+  ];
+
+  queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+  VALUES ($1, $2, $3, $4);
+  `;
+
+  pool
+    .query(queryText, newFeedback)
+    .then((response) => {
+      console.log(response);
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.log(`ERROR in POST /feedback:`, err);
+      res.sendStatus(500);
+    });
+});
+
 module.exports = router;
