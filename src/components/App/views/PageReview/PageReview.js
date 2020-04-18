bd38d0283a1feb209c9c80c25653429258dbf25d
cchelton <59346941+cchelton@users.noreply.github.com>
2020-04-18 17:04:14 -0500

    content displays on review screen

--- src/components/App/views/PageReview/PageReview.js
+++ src/components/App/views/PageReview/PageReview.js
@@ -1,16 +1,57 @@
 // review and submit here. links back to previous pages. probably will need to reset redux store on submit
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import NextButton from "../../../NextButton/NextButton";
 
 class PageReview extends Component {
   render() {
+    const newFeedback = this.props.store.newFeedbackReducer;
+    console.log(newFeedback);
+
     return (
       <div>
         <h2>Review</h2>
+
+        <table>
+          <tbody>
+            {newFeedback.feeling !== "" && (
+              <tr>
+                <td>How are you feeling today?</td>
+                <td>{newFeedback.feeling}</td>
+              </tr>
+            )}
+
+            {newFeedback.understanding !== "" && (
+              <tr>
+                <td>How well are you understanding the content?</td>
+                <td>{newFeedback.understanding}</td>
+              </tr>
+            )}
+
+            {newFeedback.support !== "" && (
+              <tr>
+                <td>How well are you being supported?</td>
+                <td>{newFeedback.support}</td>
+              </tr>
+            )}
+
+            {newFeedback.comments !== "" && (
+              <tr>
+                <td>Did you have any comments?</td>
+                <td>{newFeedback.comments}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+
         <NextButton nextPath="/thanks" />
       </div>
     );
   }
 }
 
-export default PageReview;
+const putStoreOnProps = (store) => ({
+  store,
+});
+
+export default connect(putStoreOnProps)(PageReview);
