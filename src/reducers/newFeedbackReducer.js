0d0de8a5463e742c301e8e5bdcf79be56c4789c5
cchelton <59346941+cchelton@users.noreply.github.com>
2020-04-18 15:46:41 -0500

    adds reducer to handle current user's input state

---
+++ src/reducers/newFeedbackReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  feeling: null,
+  understanding: null,
+  support: null,
+  comments: null,
+};
+
+const reducer = (state = initialState, action) => {
+  return state;
+};
+
+export default reducer;
