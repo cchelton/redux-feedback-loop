1b48ca6ad599d1fccbdfa28e033e1dfbd0fff34e
cchelton <59346941+cchelton@users.noreply.github.com>
2020-04-18 16:49:33 -0500

    adds textarea to comments page

--- src/components/App/views/PageComments/PageComments.js
+++ src/components/App/views/PageComments/PageComments.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import NextButton from "../../../NextButton/NextButton";
+import TextInput from "../../../TextInput/TextInput";
 
 class PageComments extends Component {
   render() {
     return (
       <div>
         <h2>COMMENTS</h2>
+        <TextInput property="comments" />
         <NextButton nextPath="/review" />
       </div>
     );
