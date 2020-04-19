d8e11c14a7f00f3d744ab19043a7e7c2c7abef88
cchelton <59346941+cchelton@users.noreply.github.com>
2020-04-19 15:19:25 -0500

    styled remaining selector pages

--- src/components/App/views/PageUnderstanding/PageUnderstanding.js
+++ src/components/App/views/PageUnderstanding/PageUnderstanding.js
@@ -2,21 +2,28 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import NextButton from "../../../NextButton/NextButton";
 import RatingSelector from "../../../RatingSelector/RatingSelector";
+import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
+import "./PageUnderstanding.css";
 
 class PageUnderstanding extends Component {
   render() {
     return (
-      <div>
-        <h2>Understanding</h2>
-        <RatingSelector
-          property="understanding"
-          value={this.props.store.newFeedbackReducer.understanding}
-        />
+      <Card className="Card">
+        <Typography variant="h5" className="cardTitle">
+          How well did you understand todays material?
+        </Typography>
+        <div className="Selector">
+          <RatingSelector
+            property="understanding"
+            value={this.props.store.newFeedbackReducer.understanding}
+          />
+        </div>
         <NextButton
-          nextPath="/support"
+          nextPath="./support"
           pageVal={this.props.store.newFeedbackReducer.understanding}
         />
-      </div>
+      </Card>
     );
   }
 }
